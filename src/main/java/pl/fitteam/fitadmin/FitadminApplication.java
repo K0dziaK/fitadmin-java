bd2c275fcbf6e3af73545993f108d4f1e13ab324
K0dziaK <32880560+K0dziaK@users.noreply.github.com>
2026-05-11 18:58:18 +0200

    Initial commit

---
+++ src/main/java/pl/fitteam/fitadmin/FitadminApplication.java
@@ -0,0 +1,13 @@
+package pl.fitteam.fitadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/fitteam/fitadmin/FitadminApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fitteam.fitadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
